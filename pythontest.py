947a19974aff0deb2a4a5b06d4ed0a604c4c3c8b
skiiiiittles <466306529@qq.com>
2019-03-27 13:56:44 +0800

    commit 4

--- pythontest.py
+++ pythontest.py
@@ -1,10 +1,2 @@
 print('welcome to python')
 print('welcome to python')
-print('welcome to python')
-
-
-print("welcome to python1")
-
-
-print("welcome to python")
-print("welcome to python")("welcome to python")("welcome to python")
